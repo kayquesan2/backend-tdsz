7195f3549e9aa31c7dba241e066a9094f1c63f40
kayquesan2 <rm552605@fiap.com.br>
2024-09-25 18:41:31 -0300

    Cadastro de Usuários e Autenticação com JWT e Bcrypt

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 const tarefaRoutes = require('./routes/tarefaRoutes');
+const userRoutes = require('./routes/userRoutes');
+const authMiddleware = require('./middlewares/authMiddleware');
 
+// Middleware para JSON
 app.use(express.json());
 
-app.use('/api', tarefaRoutes);
+// Rotas
+app.use('/api', userRoutes); // Rota para autenticação
+
+// Rotas protegidas
+app.use('/api/tarefas', authMiddleware, tarefaRoutes);
 
 module.exports = app;
