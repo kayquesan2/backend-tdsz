4fa936db49ee7604c141cc76bcce9ea94db61065
kayquesan2 <kayque.ferreirasan@gmail.com>
2024-10-15 18:55:02 -0300

    Instalação e Adição do CORS

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const tarefaRoutes = require('./routes/tarefaRoutes');
 const userRoutes = require('./routes/userRoutes');
 const authMiddleware = require('./middlewares/authMiddleware');
 
-// Middleware para JSON
+
 app.use(express.json());
 
-// Rotas
-app.use('/api', userRoutes); // Rota para autenticação
+app.use(cors()); 
 
-// Rotas protegidas
+app.use('/api', userRoutes); 
+
 app.use('/api/tarefas', authMiddleware, tarefaRoutes);
 
 module.exports = app;
